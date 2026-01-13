f6e6e4c3adc2e7e8391198296af394c4f76022cc
Nitish Maurya <nitishmaurya2255@gmail.com>
2026-01-14 04:06:18 +0530

    wishlist bugs fixed

--- frontend/src/components/Products/ProductsGrid.jsx
+++ frontend/src/components/Products/ProductsGrid.jsx
@@ -1,35 +1,56 @@
     import React, { useState } from 'react'
     import { useNavigate } from 'react-router-dom'
-    import { FaHeart } from "react-icons/fa";
-    import { prefetchDNS } from 'react-dom';
     import { useEffect } from 'react';
     import axios from 'axios';
     import { useSelector } from 'react-redux';
-import { toast } from 'sonner';
+    import { toast } from 'sonner';
+    import { FaHeart } from 'react-icons/fa6';
 
     const ProductsGrid = ({products,loading,error}) => {
         const {user}=useSelector((state)=>state.auth)
         const navigate=useNavigate()
         const userId=user?._id;
-        const [id,setId]=useState("");
-        const [done,setDone]=useState(false);
         
-        console.log(id)
+        
+        
+        
     
         const [favourites,setFavourites]=useState([]);
-         const handleWishList= async(userId,productId)=>{
-            setDone(true);
-            const res=await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/wishlist/add`,{userId,productId});
-            setDone(false);
+
+        const handleWishList= async(userId,productId)=>{
+           
+            const alreadyInWishlist=favourites.includes(productId)
+
+            if(alreadyInWishlist){
+                setFavourites(prev=>prev.filter(id=>id!=productId));
+            }
+            else{
+                setFavourites(prev=>[...prev,productId]);
+            }
+            
+            try {
+                const res=await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/wishlist/add`,{userId,productId});
+            
             if(res.data.message==="Added"){
+                
                  toast.success("Added to wishlist",{
                 duration:1000
             })
             }
             else{
-                 toast.success("Removed to wishlist",{
+                
+                 toast.success("Removed from wishlist",{
                 duration:1000
             })
+            }
+            } catch (error) {
+                if(alreadyInWishlist){
+                    setFavourites(prev=>[...prev,productId]);
+                }
+                else{
+                    setFavourites(prev=>prev.filter(id=>id!==productId))
+                }
+                toast.error("Something went Wrong")
             }
            
             
@@ -49,7 +70,7 @@ import { toast } from 'sonner';
                 getWishList(userId);
                 
 
-            },[done])
+            },[userId])
         
         if(loading){
             return <p>Loading...</p>
@@ -60,28 +81,32 @@ import { toast } from 'sonner';
         }
         
         const onProductClick=(id)=>{
-            setId(id)
             navigate(`/product/${id}`)
             window.scrollTo({ top: 0, behavior: 'smooth' });
         }
- 
+       
        
             
     return (
         <div className='grid grid-cols-2 md:grid-cols-4  w-full  '>
                 {
-                    products.map((product,index)=>{
+                    products.map((product)=>{
                             return(
-                                <div key={index} className='w-full md:h-[400px]  p-1 hover:shadow-lg cursor-pointer ' onClick={()=>{onProductClick(product._id)}}>
+                                <div key={product._id} className='w-full md:h-[400px]  p-1 hover:shadow-lg cursor-pointer ' onClick={()=>{onProductClick(product._id)}}>
                                     <div className='relative'>
-                                        <img loading='lazy' className=' w-full h-[200px] md:h-[320px]  object-cover  object-top' src={product.images[0].url}/>
+                                        <img loading='lazy' className=' w-full h-[200px] md:h-[320px]  object-cover  object-top' src={product.images?.[0]?.url||"/placeholder.png"}/>
                                         <FaHeart onClick={(e)=>{
                                             e.stopPropagation();
+
+                                            if(!user){
+                                                toast.error("Please Login to Add items in Wishlist")
+                                                return ;
+                                            }
                                             
                                                 handleWishList(user._id,product._id)
                                             
-                                            }} className={`top-3 right-3 text-[25px] absolute  } ${favourites.includes(product._id)?"text-red-400":"text-gray-400"} `}/>
-                                        </div>
+                                            }} className={`top-3 right-3 text-[25px] absolute ${favourites.includes(product._id)?"text-red-400":"text-gray-400"} `}/>
+                                        </div>  
                                     <div className='p-2'>
                                         <h3 className='text-[13px] truncate'>{product.name}</h3>
                                         <p>₹{product.price}</p>
