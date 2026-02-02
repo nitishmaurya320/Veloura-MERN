74ecbe51ae67d39b252c88c3d53dae0a38f19528
Nitish Maurya <nitishmaurya2255@gmail.com>
2026-02-02 19:56:42 +0530

    opacity addded

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -111,7 +111,7 @@
         
     return (
         <div className='mt-[100px] h-full    '>
-            {isPaymentDone&&<div className='fixed inset-0 z-50 flex items-center justify-center bg-black'>
+            {isPaymentDone&&<div className='fixed inset-0 z-50 flex items-center justify-center bg-black/35'>
                  <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-blue-500"></div>
             </div>}
             {/* {left} */}
